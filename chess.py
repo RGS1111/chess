40190b701f9a7ea24a0a3f603a4f4e454594c8a1
Richard Shiju <shijurt@cardiff.ac.uk>
2026-09-14 23:04:50 +0100

    Partial king_in_check implementation

--- chess.py
+++ chess.py
@@ -1,13 +1,19 @@
 # Python Chess game 
 import numpy as np
-play=True
-while play:
-    player_in = tuple(input("Enter initial coordinates: "))
-    print("mock move made:", player_in)
-    if player_in == ('0','0'):
-        play=False
-    # only needs to handle single digit as chess coordinates are 8*8 max, so (0,0) -> (8,8)  
-    int_list = [int(char) for char in player_in if char.isdigit()]
-    print("Int only list after: ", int_list)
-    print("The validation works succesfully, and only appends int to the list: ", type(int_list[0]))
-    print(abs(2-3))
+import itertools
+# play=True
+# while play:
+#     player_in = tuple(input("Enter initial coordinates: "))
+#     print("mock move made:", player_in)
+#     if player_in == ('0','0'):
+#         play=False
+#     # only needs to handle single digit as chess coordinates are 8*8 max, so (0,0) -> (8,8)  
+#     int_list = [int(char) for char in player_in if char.isdigit()]
+#     print("Int only list after: ", int_list)
+#     print("The validation works succesfully, and only appends int to the list: ", type(int_list[0]))
+#     print(abs(2-3))
+
+numbers = [1, 2, -1, -2]
+pairs = itertools.permutations(numbers, 2)
+filtered_pairs = [rook_pos for rook_pos in pairs if abs(rook_pos[0]) != abs(rook_pos[1])]
+print(filtered_pairs)
